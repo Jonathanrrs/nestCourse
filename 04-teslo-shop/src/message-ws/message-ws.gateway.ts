9fb41daba6a4c21aadafb8b927a4d63c40181a10
Jonathanrrs <yoni_12rrs@hotmail.com>
2023-01-18 14:42:41 -0600

    Server - Mantener identificados los clientes

--- 04-teslo-shop/src/message-ws/message-ws.gateway.ts
+++ 04-teslo-shop/src/message-ws/message-ws.gateway.ts
@@ -1,7 +1,24 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { MessageWsService } from './message-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessageWsGateway {
+export class MessageWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messageWsService: MessageWsService) {}
+  handleConnection(client: Socket) {
+    // console.log('client connected', client.id);
+    this.messageWsService.registerClient(client);
+    console.log(this.messageWsService.getConnectedClients());
+  }
+  handleDisconnect(client: Socket) {
+    // console.log('client disconnected', client.id);
+    this.messageWsService.removeClient(client.id);
+    console.log(this.messageWsService.getConnectedClients());
+  }
 }
