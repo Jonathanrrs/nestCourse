936b95f0539c31b085e98a01c0022362ef06ab07
Jonathanrrs <yoni_12rrs@hotmail.com>
2023-01-18 14:32:01 -0600

    Cliente - Vite Vanilla TypeScript

---
+++ 05-ws-client/src/main.ts
@@ -0,0 +1,10 @@
+import { connectToServer } from "./socket-client";
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+   <h1>Websocket - client <h1/>
+  </div>
+`;
+
+connectToServer();
